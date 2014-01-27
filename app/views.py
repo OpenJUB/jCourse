e2d790ea415423116c417210044156b53cc6b4ec
dhasegan <danielhasegan@gmail.com>
2014-01-27 11:17:53 -0500

    Display all courses in the DB on the front page

--- app/views.py
+++ app/views.py
@@ -1,9 +1,15 @@
 
 from django.shortcuts import render, redirect, get_object_or_404
 
+from app.models import *
+
 def home(request):
     context = {
         "page": "home",
     }
+
+    # Get courses
+    courses = Course.objects.all()
+    context['courses'] = courses
 
     return render(request, "pages/home.html", context)
