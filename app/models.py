ed4240b5f5d27733eaf326e9d9331eb4d78ef39e
dhasegan <danielhasegan@gmail.com>
2014-01-27 10:31:23 -0500

    Populated the DB with 95% of the courses. please do not change models

--- app/models.py
+++ app/models.py
@@ -1,51 +1,64 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+from django.conf import settings 
+
 LECTURE = 'LEC'
 SEMINAR = 'SEM'
 PROJECT = 'PRJ'
 WORKSHOP = 'WKS'
 LAB = 'LAB'
+UNKNOWN = 'UNK'
 COURSE_TYPES = (
     (LECTURE, 'Lecture'),
     (SEMINAR, 'Seminar'),
     (PROJECT, 'Project'),
     (WORKSHOP, 'APS Workshop'),
-    (LAB, 'Lab')
+    (LAB, 'Lab'),
+    (UNKNOWN, 'Unknown')
 )
+
+class jUser(User):
+    department = models.CharField(max_length=50)
 
 class Professor(models.Model):
     name = models.CharField(max_length=50)
     department = models.CharField(max_length=50, blank=True, null=True)
 
     def __unicode__(self):
-        return str(name)
+        return str(self.name)
 
-class Professor_Rating(models.Model):
-    prof = models.ForeignKey('Professor')
+class Rating(models.Model):
+    user = models.ForeignKey('jUser')
     course = models.ForeignKey('Course')
     rating = models.FloatField()
 
     def __unicode__(self):
-        return "Rating " + str(prof.name)
+        return str(self.rating)
+
+class Professor_Rating(Rating):
+    prof = models.ForeignKey('Professor')
+
+    def __unicode__(self):
+        return "Rating " + str(self.prof.name)
 
 class Course(models.Model):
-    course_id = models.IntegerField()
+    course_id = models.CharField( max_length=10 )
     course_type = models.CharField( max_length=3,
                                     choices=COURSE_TYPES,
                                     default=LECTURE)
     name = models.CharField(max_length=100)
     instructors = models.ManyToManyField('Professor')
     credits = models.FloatField()
-    description = models.CharField(max_length=2000)
+    description = models.CharField(max_length=2000, blank=True, null=True)
     additional_info = models.CharField(max_length=2000, blank=True, null=True)
     sections_info = models.CharField(max_length=2000, blank=True, null=True)
     catalogue = models.CharField(max_length=300)
     grades = models.CharField(max_length=300, blank=True, null=True)
-    abreviation = models.CharField(max_length=50, blank=True, null=True)
+    grades_info = models.CharField(max_length=2000, blank=True, null=True)
+    abbreviation = models.CharField(max_length=50, blank=True, null=True)
     participants = models.CharField(max_length=10, blank=True, null=True)
     hours_per_week = models.CharField(max_length=10, blank=True,null=True)
 
-    general_rating = models.FloatField(blank=True, null=True)
-
     def __unicode__(self):
-        return str(name)
+        return str(self.name)
--- crawler/parser.py
+++ crawler/parser.py
@@ -84,33 +84,36 @@ def cleanuper(content):
 
 importantFields = [ 'Instructors', 'Type', 'Org-unit', 'Course Name Abbreviation', 'Hours per week',
                     'Credits', 'Min. | Max. participants', 'Partial Grades', 'Official Course Description',
-                    'Additional Information', 'This course is divided into the following sections']
+                    'Additional Information', 'This course is divided into the following sections',
+                    'Further Grading Information']
+linksFile = open('crawler/courses', 'r')
+namesFile = open('crawler/courseNames', 'r')
+outputFile = open('crawler/courseDetails', 'w')
 
+coursesList = []
 
-linksFile = open('courses', 'r')
-namesFile = open('courseNames', 'r')
-outputFile = open('courseDetails', 'w')
-
+# Parse the Links
 for link in linksFile:
+    # Get link and print progress
     courseName = namesFile.readline()
     print courseName
-
+    # Download the page
     page = urllib.urlopen(link)
     page = page.read()
-
+    # Parse the page
     parser = MyHTMLParser()
     parser.feed(page)
     uglyCourseInfo = parser.getCourseInfo()
-
+    # Delete the extra stuff and make it a dictionary
     courseInfo = dict(map(lambda x: (cleanuper(x[0]),cleanuper(x[1])), uglyCourseInfo.iteritems() ))
     if not 'Course offering details' in courseInfo:
         print "Error: No label 'Course offering details' abort scanning page"
     if not 'Name' in courseInfo:
         print "Error: No label 'Name' abort scanning page"
-
+    # Get main details about the course
     details = courseInfo['Course offering details']
     detailsMap = {}
-
+    # For each field found put it in the map
     for field in importantFields:
         length = len(details)
         if (field+':') in details:
@@ -118,21 +121,86 @@ for link in linksFile:
             stopIdx = min( [ (details.find(f+":") if details.find(f+":") >= startIdx else length) for f in importantFields] )
             if startIdx <= stopIdx:
                 if startIdx < stopIdx:
-                    detailsMap[field] = details[startIdx:stopIdx]
+                    detailsMap[field] = unicode(details[startIdx:stopIdx], errors='ignore').strip()
             else:
                 print "Error: Start after stop"
-
+    # Parse the Catalogue information
     if 'Contained in course catalogues' in courseInfo:
         catalogue = courseInfo['Contained in course catalogues']
         startIdx = catalogue.find('> ')
         detailsMap['Catalogue'] = catalogue[startIdx+2:]
     else:
         print "Error: No label 'Contained in course catalogues'"
-
     detailsMap['Name'] = courseName[:-1]
+    # Parse for the ID and just course name
+    courseID = detailsMap['Name'][0:6]
+    courseNameOnly = detailsMap['Name'][7:]
+    detailsMap['CourseID'] = courseID
+    detailsMap['CourseName'] = courseNameOnly
+    # Add to our list
+    coursesList.append(detailsMap)
 
-    for k,v in detailsMap.iteritems():
-        outputFile.write(k + "::: " + v + "\n")
-    outputFile.write("\n\n")
-
+linksFile.close()
+namesFile.close()
 outputFile.close()
+
+
+# Insert results into the DB
+
+from app.models import *
+
+for courseDetails in coursesList:
+    print courseDetails['CourseName']
+    if len(Course.objects.filter(name=courseDetails['CourseName'])) > 0:
+        continue
+    # Setup instructors
+    dbProfs = []
+    instructors = courseDetails['Instructors'].split("; ")
+    for instructor in instructors:
+        prof = False
+        profs = Professor.objects.filter(name=instructor)
+        if len(profs) > 0:
+            prof = profs[0]
+        else:
+            prof = Professor(name=unicode(instructor))
+            prof.save()
+        if prof:
+            dbProfs.append(prof)
+    # Get Course type
+    ctype = False
+    for CTYPE in COURSE_TYPES:
+        if 'Type' in courseDetails and CTYPE[1] == courseDetails['Type']:
+            ctype = CTYPE[0]
+    if not ctype:
+        print "Error! Didnt find course type " + courseDetails['CourseName'] + " in our models!"
+        ctype = UNKNOWN
+    # Get Credits number
+    if 'Credits' in courseDetails:
+        credits = float(courseDetails['Credits'])
+    else:
+        credits = 5.0
+    # Create the Course class
+    course = Course(course_id = courseDetails['CourseID'],
+                    course_type = ctype,
+                    name = courseDetails['CourseName'],
+                    credits = credits,
+                    catalogue = courseDetails['Catalogue'],
+                    abbreviation = courseDetails['Course Name Abbreviation'])
+    if 'Official Course Description' in courseDetails:
+        course.description = courseDetails['Official Course Description']
+    if 'Min. | Max. participants' in courseDetails and courseDetails['Min. | Max. participants'] != '- | -':
+        course.participants = courseDetails['Min. | Max. participants']
+    if 'Hours per week' in courseDetails:
+        course.hours_per_week = courseDetails['Hours per week']
+    if 'Partial Grades' in courseDetails and courseDetails['Partial Grades'] != "":
+        course.grades = courseDetails['Partial Grades']
+    if 'Additional Information' in courseDetails:
+        course.additional_info = courseDetails['Additional Information']
+    if 'This course is divided into the following sections' in courseDetails:
+        course.sections_info = courseDetails['This course is divided into the following sections']
+    if 'Further Grading Information' in courseDetails:
+        course.grades_info = courseDetails['Further Grading Information']
+    course.save()
+    for dbProf in dbProfs:
+        course.instructors.add(dbProf)
+
