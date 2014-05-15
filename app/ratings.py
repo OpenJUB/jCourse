0bb9c2b6b68d2b962e4c01dafc7a9dcb7232aab7
dhasegan <danielhasegan@gmail.com>
2014-05-15 10:05:55 -0400

    Changed the rating score of comments

--- app/ratings.py
+++ app/ratings.py
@@ -7,7 +7,10 @@ def comment_rating(upvotes, downvotes):
     if total <= 0:
         return 0
 
-    Z = 1.96 # Zindex Assumes .95 confidence
+    if upvotes == 0 and downvotes == 1:
+        return -0.01
+
+    Z = 1.6 # Zindex Assumes .95 confidence
     phat = 1.0 * upvotes / total
     WilsonScore = (phat + Z*Z/(2*total) - Z * math.sqrt(phat*(1-phat) + Z*Z/(4*total))) / (1+Z*Z/total)
 
